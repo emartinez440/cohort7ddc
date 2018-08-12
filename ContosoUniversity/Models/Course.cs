41d835eb5b36ee208932f3f6e1071897df48f3c5
Eva Martinez <neutronpurple@yahoo.com>
2018-08-12 09:38:56 -0600

    Initial

---
+++ ContosoUniversity/Models/Course.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+
+        public int CourseID
+       { get; set; }
+
+        public string Title { get; set; }
+
+        public int Credits { get; set; }
+
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,22 @@
+﻿namespace ContosoUniversity.Models
+{
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+
+        public int CourseID { get; set; }
+
+        public int StudentID { get; set; }
+
+        public Grade? Grade { get; set; }
+
+        public Models.Course Course { get; set; }
+
+        public Student Student { get; set; }
+    
+    }
+}
+    public enum Grade
+{
+    A,B,C,D,F
+}
